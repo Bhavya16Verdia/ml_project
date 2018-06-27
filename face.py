8d7f9a50298bc25be585b09831b30f550b436cbc
Bhavya16Verdia <39920884+Bhavya16Verdia@users.noreply.github.com>
2018-06-27 14:38:31 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import cv2
+import numpy as np
+
+face_cascade=cv2.CascadeClassifier('detect_face.xml')
+cap=cv2.VideoCapture(0)
+while True:
+	status,img=cap.read()
+	grayimg=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	face=face_cascade.detectMultiScale(grayimg,1.15,5)
+	for(x,y,w,h) in face:
+		cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+		roi_gray=grayimg[y:y+h,x:x+w]
+		roi_color=img[y:y+h,x:x+w]
+
+	cv2.imshow('imgw',img)
+	k=cv2.waitKey(30) & 0xFF
+	if k == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+	
